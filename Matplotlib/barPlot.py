e8a0023e5afdf31d4fd1e8c7077a3daf9e13f3b0
ashikapl <ashikapanchal54@gmail.com>
2024-09-20 11:12:46 +0530

    Matplotlib

--- Matplotlib/barPlot.py
+++ Matplotlib/barPlot.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
 
 a = ['Python','C++','Java','Js']
-b = [60,50,45,42]
+b = [87,70,60,82]
+c = [20,30,40,50]
 
 
-l1 = plt.xlabel('Language',fontsize=20)
-l2 = plt.ylabel('No.',fontsize=20)
-t = plt.title('Ranks',fontsize=20)
-x = plt.bar(a,b,width=0.5)
+l1 = plt.xlabel('Language',fontsize=15)
+l2 = plt.ylabel('No.',fontsize=15)
+t = plt.title('Ranks',fontsize=15)
+c = ['b','g','r','y']
+# x = plt.bar(a,b,width=0.5, color=c, align='edge', edgecolor='black', linewidth=2, linestyle=':',alpha=0.9)
+# alpha value is use btwn 0 to 1 for brightness
+# linewidth is for boundary line of graph plots and
+# linestyle is for dotted or simple line around the plots
+# we can use center also in align parameter to keep the (a) list point at center
+width=0.5
+x = plt.bar(a,b,width, color='r', edgecolor = 'black', label="Popularity")
+x = plt.bar(a,c,width, color='b', edgecolor = 'green', label="Popularity1")
+plt.legend()
 plt.show()
