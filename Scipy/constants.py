583768265ae8b3da35a2ec99254c1025a0bdf463
ashikapl <ashikapanchal54@gmail.com>
2024-09-29 22:55:36 +0530

    Scipy

---
+++ Scipy/constants.py
@@ -0,0 +1,12 @@
+# Constansts in Scipy
+
+# from scipy.constants import pi,golden_ratio
+# print(f"Pi: {pi}")
+# print(f"Golden_ratio: {golden_ratio}")
+# radius = 22
+# print("Area of Circle:", (pi*radius*radius))
+
+from scipy.constants import m_n, Avogadro
+print("Mass of neutron:", m_n, "Kg")
+print("Avogadro No:", Avogadro)
+
---
+++ Scipy/intro.py
@@ -0,0 +1,10 @@
+# Scipy  Here constants if a submodule of Scipy 
+import scipy
+from scipy import constants 
+# print(scipy.__version__)
+print(constants.liter)
+print("Pi:",constants.pi)
+# print(dir(constants))
+print("Yotta:",constants.yotta)
+print("Minute:",constants.minute)
+print("kilogram_force",constants.kilogram_force)
