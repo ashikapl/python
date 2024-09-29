eca20cdc31d3ccbdf6e16038a3ac834d203cd896
ashikapl <ashikapanchal54@gmail.com>
2024-09-29 17:36:27 +0530

    Matplotlib

---
+++ Matplotlib/test.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [11,13,15,23,45,12,22]
+y = [1,10,23,24,26,47,16]
+plt.scatter(x,y)
+
+x = [15,18,20,22,27,66,77]
+y = [1,2,3,4,5,6,7]
+plt.scatter(x,y)
+
+plt.show()
